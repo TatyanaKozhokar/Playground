0841c7bac841432865f1cba91c6af41514abc171
Tatyana Kozhokar <tz5799@gmail.com>
2026-03-18 11:18:33 +0300

    Добавила еще несколько

--- src/test/java/PlaygroundTest.java
+++ src/test/java/PlaygroundTest.java
@@ -115,4 +115,89 @@ public class PlaygroundTest {
         assertEquals(newName, newButton);
 
     }
+
+    @Test
+    void scrollbars() {
+        Page page = PlaywrightManager.getPage();
+        page.navigate("http://uitestingplayground.com/scrollbars");
+        Locator button = page.locator("//button[@id='hidingButton']");
+
+        button.scrollIntoViewIfNeeded();
+        button.click();
+
+    }
+
+    //Не доделано
+    @Test
+    void dynamicTable() {
+        Page page = PlaywrightManager.getPage();
+        page.navigate("http://uitestingplayground.com/dynamictable");
+        String tableCpu = page.locator(
+                "//span[@role='cell' and contains(text(), 'Chrome')]/following-sibling::span[@role='cell'][1]"
+        ).textContent();
+
+        String labelText = page.locator("//p[@class='bg-warning']").textContent();
+        String labelCpu = labelText.split(":")[1].trim();
+
+        assertEquals(labelCpu, tableCpu);
+
+    }
+
+    @Test
+    void verifyText() {
+        Page page = PlaywrightManager.getPage();
+        page.navigate("http://uitestingplayground.com/verifytext");
+        Locator element = page.locator("//span[normalize-space()='Welcome UserName!']");
+        element.isVisible();
+    }
+
+    @Test
+    void progressbar() {
+        Page page = PlaywrightManager.getPage();
+        page.navigate("http://uitestingplayground.com/progressbar");
+        page.locator("//button[@id='startButton']").click();
+        Locator progressBar = page.locator("//div[@id='progressBar']");
+
+        while (true) {
+            String progressValue = progressBar.getAttribute("aria-valuenow");
+            int progress = Integer.parseInt(progressValue);
+
+            if (progress >= 75) {
+                break;
+            }
+            page.waitForTimeout(100);
+        }
+
+        page.locator("//button[@id='stopButton']").click();
+
+        String finalProgress = progressBar.getAttribute("aria-valuenow");
+        System.out.println("Прогресс остановлен на " + finalProgress + "%");
+    }
+
+    @Test
+    void visibility() {
+        Page page = PlaywrightManager.getPage();
+        page.navigate("http://uitestingplayground.com/visibility");
+        String[][] buttons = {
+                {"hideButton"},
+                {"removedButton"},
+                {"zeroWidthButton"},
+                {"overlappedButton"},
+                {"transparentButton"},
+                {"invisibleButton"},
+                {"notdisplayedButton"},
+                {"offscreenButton"}
+        };
+
+        page.locator("//button[@id='hideButton']").click();
+        page.waitForTimeout(1000);
+
+        for (String[] button : buttons) {
+            String id = button[0];
+            String xpath = "//button[@id='" + id + "']";
+
+            page.locator(xpath).isVisible();
+
+        }
+    }
 }
